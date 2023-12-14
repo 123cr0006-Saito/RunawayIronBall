e184aefa63a9b4af21f073da0fa4aae7811a54b9
123CR0013 <123cr0013@tokyo.amg.ac.jp>
2023-12-14 18:49:59 +0900

    Chain.cppとChain.hの追加

---
+++ Game/Game/source/Chain.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "appframe.h"
+
+
+#define CHAIN_MAX 12
+
+class Chain {
+public:
+
+	void Init();
+	void Process(VECTOR playerPos);
+	void AnimProcess();
+
+	void Render();
+
+	void DrawDebugInfo();
+
+
+private:
+	XInput* _input;
+
+	int _modelHandle[CHAIN_MAX];
+
+	VECTOR _pos[CHAIN_MAX];
+	VECTOR _mbDir;
+
+
+
+	int _attackCnt;
+
+
+	int _animIndex;
+	float _animTotalTime;
+	float _playTime;
+
+
+	float _cnt;
+
+	MATRIX _m[CHAIN_MAX];
+
+	float _length;
+};
