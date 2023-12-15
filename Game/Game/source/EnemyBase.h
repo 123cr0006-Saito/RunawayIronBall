c7f8c1264c589341110856790b434b4a0a7af903
123cr0006-Saito <123cr0006@tokyo.amg.ac.jp>
2023-12-15 16:24:37 +0900

    slablockにあった敵AIを基底クラスに移動
    デバックでslablockに作ってあった敵AIを基底クラスのenemybaseに移動

--- Game/Game/source/EnemyBase.h
+++ Game/Game/source/EnemyBase.h
@@ -4,8 +4,8 @@
 #include "math.h"
 class EnemyBase
 {
-public: 
-	EnemyBase();
+public:
+	EnemyBase(int model, VECTOR pos);
 	~EnemyBase();
 
 	virtual bool Process();
@@ -13,28 +13,38 @@ public:
 
 	virtual bool StopPos();
 
-	virtual void CollisionDetection();
-
-protected :
+protected:
+	Player* _player;
 	VECTOR _pos;//エネミーの座標
 	float _direction;//y軸の向いている方向
 	float _sartchRange;//聴覚範囲の半径
-	float _flontAngle;//視界範囲の角度
 	float _moveRange;//移動範囲の半径
-	int _model;//モデル
-	int _speed;//移動速度
-	float _r;//半径
+	int    _model;//モデル
+	int    _speed;//移動速度
+	float _r;//当たり判定用の半径
 
 	float _stopTime;//停止している時間
-	int _arriveTime;//移動地点についた時の時間
+	int    _arriveTime;//移動地点についた時の時間
 
 	VECTOR _orignPos;//出現地点
 	VECTOR _nextMovePoint;//次の移動地点
 
+	static const float _flontAngle;//視界範囲の角度
+	float _sartchRangeSize;//索敵範囲の半径
+	float _discoverRangeSize;//発見時、対象の見失うまでの距離の半径;
+
+	//回旋させるための変数
+	float _nextDir;
+	float _oldDir;
+
+	//イージングさせるための変数
+	int _easingFrame;
+	VECTOR _saveNextPoint;
 
 	enum TYPE : int {
 		search,
-		discover
+		discover,
+		attack
 	};
 
 	TYPE _state;//今の状態
--- Game/Game/source/SlaBlock.h
+++ Game/Game/source/SlaBlock.h
@@ -2,26 +2,17 @@
 #include "EnemyBase.h"
 #include "Player.h"
 #include <string>
-class SlaBlock :  public EnemyBase
+class SlaBlock : public EnemyBase
 {
 public:
-	SlaBlock(std::string model_name,VECTOR pos, Player* player);
+	SlaBlock(int model, VECTOR pos, Player* player);
 	~SlaBlock();
 
 	virtual bool Process() override;
 	virtual bool Render() override;
 
 protected:
-	Player*  _player;
-	static const float _sartchRangeSize;//索敵範囲の半径
-	static const float _discoverRangeSize;//発見時、対象の見失うまでの距離の半径
+	static const float _fixSartchSize;//索敵範囲の半径
+	static const float _fixDiscoverSize;//発見時、対象の見失うまでの距離の半径
 
-	//回旋させるための変数
-	float _nextDir;
-	float _oldDir;
-
-	//イージングに使用
-	int _easingFrame;
-	VECTOR _saveNextPoint;
 };
-
