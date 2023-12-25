1eedeb2ce27d7deb2038ac5899f142cb58ea41d7
saito_ko <123cr0006@tokyo.amg.ac.jp>
2023-12-25 15:02:42 +0900

    タイトル画面の機能追加
    タイトル画面に
    開始、設定、終了の機能を追加
    今は開始しかできていない。

--- Game/Game/source/ModeTitle.cpp
+++ Game/Game/source/ModeTitle.cpp
@@ -5,29 +5,82 @@
 #include "ModeGame.h"
 #include "math.h"
 
+
 bool ModeTitle::Initialize() {
 	if (!base::Initialize()) { return false; }
 
+	_input = XInput::GetInstance();
+	_modeCount = 0;
 	return true;
 }
 
 bool ModeTitle::Terminate() {
-	// _cg を解放しないといけないけど、AMLibは個別解放できない・・
 	base::Terminate();
 	return true;
 }
 
+void ModeTitle::SelectGameStart() {
+	ModeServer::GetInstance()->Del(this);
+	ModeServer::GetInstance()->Add(new ModeGame(), 1, "game");
+};
+
+void ModeTitle::SelectOption() {
+
+};
+
+void ModeTitle::SelectGameEnd() {
+
+};
+
 bool ModeTitle::Process() {
 	base::Process();
+	int count = 0;
 
+	//モード選択の切り替え
+	if (_input->GetTrg(XINPUT_BUTTON_DPAD_UP)) {
+		count--;
+	}
+	else if (_input->GetTrg(XINPUT_BUTTON_DPAD_DOWN)) {
+		count++;
+	}
 
+	_modeCount += count;
+	_modeCount = _modeCount % 3;
+
+	if (_modeCount < 0) {
+		_modeCount = 2;
+	}
+
+	//モードの選択
+	if (_input->GetTrg(XINPUT_BUTTON_A)) {
+		switch (_modeCount) {
+		case 0:
+			SelectGameStart();
+			break;
+		case 1:
+			SelectOption();
+			break;
+		case 2:
+			SelectGameEnd();
+			break;
+		}
+	}
 
 	return true;
 }
 
 bool ModeTitle::Render() {
 	base::Render();
+	clsDx();
+	printfDx("%d", _modeCount);
+	int x = 1920 / 2;
+	int y = 1080 / 2;
+
+	for (int i = 0; i < 3; i++) {
+		int color = GetColor(255, 0, 0);
+		if (i == _modeCount) { color = GetColor(0, 255, 0); }
+		DrawBox(x - 200, y - 50 + i * 200, x + 200, y + 50 + i * 200, color, true);
+	}
 
 	return true;
 }
-
