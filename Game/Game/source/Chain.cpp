0c321d308a8b0af69941f1f184e17c2bcff57cf7
123CR0013 <123cr0013@tokyo.amg.ac.jp>
2024-01-12 14:44:29 +0900

    振り回し時と追従時で鎖の挙動を変更するように修正。

--- Game/Game/source/Chain.cpp
+++ Game/Game/source/Chain.cpp
@@ -30,134 +30,87 @@ void Chain::Init() {
 	_mbDir = VGet(0, 0, 0);
 
 
-	_attackCnt = 0;
+	_attackAnimCnt = 0;
 
 	_cnt = 0.0f;
+	_attackDir = 1;
 
 	for (int i = 0; i < CHAIN_MAX; i++) {
 		_m[i] = MGetIdent();
 	}
 
-	_length = 80.0f;
+	_length = 50.0f;
 
 	_isSwing = false;
+
+
+	_playerInstance = Player::GetInstance();
+	_playerModelHandle = _playerInstance->GetModelHandle();
 }
 
 void Chain::Process(VECTOR playerPos) {
-//	float gravity = 8.0f;
-//
-//
-//	VECTOR vOld = VSub(_pos[1], _pos[0]);
-//	VECTOR vBase = VGet(0.0f, 0.0f, -100.0f);
-//
-//#if 0
-//	vBase = VTransform(vBase, MGetRotY(DegToRad(40.0f) * sin(_cnt)));  // wave
-//	_pos[1] = vBase;
-//	_cnt += (2 * DX_PI) / (60.0f); // wave
-//#else
-//	vBase = VTransform(vBase, MGetRotY(DegToRad(_cnt))); // swing
-//	//_pos[1] = vBase;
-//	_cnt += 8.0f;					 // swing
-//#endif	
+	_isSwing = _playerInstance->GetIsSwing();
 
-
-
-	//VECTOR vDir = VSub(_pos[1], _pos[0]);
-	//MATRIX mRot = MGetRotY(DegToRad(_cnt))/*MGetRotVec2(VGet(0.0f, 0.0f, -100.0f), vDir)*/;
-	
-	
-	
 	_pos[0] = playerPos;
 	if (_isSwing) {
-		MATRIX mRot = MGetRotY(Math::DegToRad(_cnt));
+		/* デバッグ用 */
+		{
+			VECTOR vOrigin = VGet(0.0f, 0.0f, 0.0f);
+			MATRIX m = MGetIdent();
+	
+			// 鎖と腕輪の連結点
+			m = MV1GetFrameLocalWorldMatrix(_playerModelHandle, 217);
+			_pos[0] = VTransform(vOrigin, m);
+	
+			// 1つ目
+			m = MV1GetFrameLocalWorldMatrix(_playerModelHandle, 218);
+			_pos[1] = VTransform(vOrigin, m);
+	
+			// 鉄球の位置
+			m = MV1GetFrameLocalWorldMatrix(_playerModelHandle, 219);
+			//VECTOR vTmp = VSub(VTransform(vOrigin, m), _pos[0]);
+			//vTmp = VNorm(vTmp);
+			//vTmp.y = 0.0f;
+			//m = MMult(m, MGetTranslate(VScale(vTmp, 1000.0f)));
 
-		for (int i = CHAIN_MAX - 1; 0 < i; i--) {
-			_m[i] = _m[i - 1];
+			_pos[CHAIN_MAX - 1] = VTransform(vOrigin, m);
 		}
-		_m[0] = mRot;
+		/* */
 
-		for (int i = 1; i < CHAIN_MAX; i++) {
-			VECTOR vTmp = VGet(0.0f, 0.0f, -100.0f * i);
-			_pos[i] = VTransform(vTmp, MMult(_m[i], MGetTranslate(_pos[0])));
-		}
+		// キャラの座標から見た一つ目の鎖を配置する方向
+		VECTOR vBase = VSub(_pos[1], _pos[0]);
 
-		_cnt += 5.0f;
-	}
-	else {
-		//MATRIX mRot = MGetRotY(Math::DegToRad(_cnt));
-		//MATRIX mIdentity = MGetIdent();
-		//for (int i = CHAIN_MAX - 1; 0 < i; i--) {
-		//	_m[i] = _m[i - 1];
-		//}
-		//_m[0] = mRot;
+		// キャラの座標から見た鉄球を配置する方向
+		VECTOR vTarget = VSub(_pos[CHAIN_MAX - 1], _pos[0]);
 
-		//for (int i = 1; i < CHAIN_MAX; i++) {
-		//	VECTOR vTmp = VGet(0.0f, 0.0f, -100.0f * i);
-		//	_pos[i] = VTransform(vTmp, MMult(_m[i], MGetTranslate(_pos[0])));
-		//}
-		//_cnt -= 5.0f;
-		//if (_cnt < 0.0f) { _cnt = 0.0f; }
-	}
 
-	// ストップ
-	if (_input->GetTrg(XINPUT_BUTTON_BACK) != 0) {
-		_isSwing = !(_isSwing);
-	}
+		float rad = Math::CalcVectorAngle(vBase, vTarget);
+		float dist = VSize(vTarget);
+		VECTOR vCross = VCross(vBase, vTarget);
+		const float chainNum = CHAIN_MAX - 2; ////////////////// （要修正）振り回している感じを出すなら、最後の鎖がピッタリ鉄球の接続位置に来るようにせず、少し引っ張っている方向にずらす必要がある
+		//rad /= chainNum;
+		for (int i = 1; i < CHAIN_MAX - 1; i++) {
+			VECTOR vTmp = VScale(VNorm(vBase), dist * ((float)(i - 1) / chainNum));
+			MATRIX mRot = MGetRotAxis(vCross, rad * ((float)(i - 1) / chainNum));
+			vTmp = VTransform(vTmp, mRot);
+			_pos[i] = VTransform(vTmp, MGetTranslate(_pos[0]));
 
-	if (CheckHitKey(KEY_INPUT_Z) != 0) {
-		_pos[0] = VGet(0, 0, 0);
-		for (int i = 0; i < CHAIN_MAX; i++) {
-			_m[i] = MGetIdent();
-			if (i > 0) {
-				_pos[i] = VAdd(_pos[i - 1], VGet(0.0f, 0.0f, -100.0f));
+			if (_pos[i].y < 0.0f) {
+				_pos[i].y = 0.0f;
 			}
 		}
-		_cnt = 0.0f;
-	}
 	
 
-	//for (int i = 1; i < CHAIN_MAX; i++) {
-	//	if (_cnt / 60.0f < i) break;
-	//	VECTOR vDir = VSub(_pos[i], _pos[i - 1]);
-	//	vDir.y = 0.0f;
-	//	if (VSize(vDir) > 0.0f) {
-	//		vDir = VNorm(vDir);
-	//		vDir = VTransform(vDir, MGetRotY(90));
-	//		_pos[i] = VAdd(_pos[i], VScale(vDir, 60.0f));
-	//	}		
-	//}
-	//_cnt++;
 
-	//VECTOR vTmp = VGet(0, 0, 0);
-	//MATRIX mTranslate = MGetTranslate(VGet())
-
-
-	//bool isMove = false;
-	//VECTOR vDir = VGet(gamePad._lStick.x, -gamePad._lStick.y, 0);
-	//if (VSize(vDir) > 0.0f) {
-	//	vDir = VNorm(vDir);
-	//	_pos[0] = VAdd(_pos[0], VScale(vDir, 10.0f));
-	//	isMove = true;
-	//}
-
-	//_pos[0] = VGet(0, 500, 0);
-
-	//VECTOR vTmp[CHAIN_MAX];
-	//for (int i = 1; i < CHAIN_MAX; i++) {
-	//	vTmp[i] = _pos[i];
-
-	//	//if (i != 1 || isMove == true) {
-	//		VECTOR vSub = VSub(_pos[i], _oldPos[i]);
-	//		if (VSize(vSub) > 0.0f) {
-	//			_pos[i] = VAdd(_pos[i], vSub);
-
-	//		}
-	//	//}
-
-	//		//_pos[i].y -= 1.0f;
-	//	_oldPos[i] = vTmp[i];
-	//}
-
+		_cnt += 3.5f * (float)_attackDir;
+		if (_cnt > 80.0f) {
+			_attackDir = -1;
+		}
+		else if (_cnt < -80.0f) {
+			_attackDir = 1;
+		}
+	}
+	else {
 	// 重力処理
 	for (int i = 1; i < CHAIN_MAX - 1; i++) {
 		_pos[i].y -= 12.0f;
@@ -166,15 +119,15 @@ void Chain::Process(VECTOR playerPos) {
 		}
 	}
 
-	_length = 50.0f;
+		//_length = 50.0f;
 	for (int i = 0; i < CHAIN_MAX - 1; i++) {
 		VECTOR vDelta = VSub(_pos[i + 1], _pos[i]);
 		float distance = VSize(vDelta);
 		float difference = _length - distance;
 
-		float offsetX = (difference * vDelta.x / distance) * 0.5f;
-		float offsetY = (difference * vDelta.y / distance) * 0.5f;
-		float offsetZ = (difference * vDelta.z / distance) * 0.5f;
+			float offsetX = (difference * vDelta.x / distance) * 0.8f;
+			float offsetY = (difference * vDelta.y / distance) * 0.8f;
+			float offsetZ = (difference * vDelta.z / distance) * 0.8f;
 
 		if (i != 0) {
 			_pos[i].x -= offsetX;
@@ -186,37 +139,7 @@ void Chain::Process(VECTOR playerPos) {
 		_pos[i + 1].z += offsetZ;
 	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	//// 外側から1つ内側のモデルを引っ張る
-	//for (int i = CHAIN_MAX - 1; 1 < i; i--) {
-	//	VECTOR sub = VSub(_pos[i], _pos[i - 1]);
-	//	while (VSize(sub) > 100.0f) {
-	//		_pos[i - 1] = VAdd(_pos[i - 1], VNorm(sub));
-	//		sub = VSub(_pos[i], _pos[i - 1]);
-	//	}
-	//}
-
-	//// 内側から1つ外側のモデルを引っ張る
-	//for (int i = 0; i < CHAIN_MAX - 1; i++) {
-	//	VECTOR sub = VSub(_pos[i + 1], _pos[i]);
-	//	while (VSize(sub) > 100.0f) {
-	//		_pos[i + 1] = VAdd(_pos[i + 1], VNorm(sub));
-	//		sub = VSub(_pos[i], _pos[i + 1]);
-	//	}
-	//}
+	}
 
 	// モデルに座標を反映させる
 	for (int i = 0; i < CHAIN_MAX; i++) {
@@ -230,9 +153,9 @@ void Chain::Process(VECTOR playerPos) {
 		_mbDir = VNorm(_mbDir);
 		Math::SetModelForward_RotationY(_modelHandle[CHAIN_MAX - 1], _mbDir);
 	}
-	_attackCnt++;
-	if (90 < _attackCnt) {
-		_attackCnt = 0;
+	_attackAnimCnt++;
+	if (90 < _attackAnimCnt) {
+		_attackAnimCnt = 0;
 	}
 
 	AnimProcess();
@@ -242,7 +165,7 @@ void Chain::AnimProcess()
 {
 	MV1SetAttachAnimTime(_modelHandle[CHAIN_MAX - 1], _animIndex, _playTime);
 
-	_playTime += _attackCnt < 60 ? 10.0f : 1.0f;	
+	_playTime += _attackAnimCnt < 60 ? 10.0f : 1.0f;	
 	if (_animTotalTime < _playTime) {
 		_playTime = 0.0f;
 	}
