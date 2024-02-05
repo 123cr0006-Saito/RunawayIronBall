dc072329db6309b2096ab4b1a1fdd3017259ae39
123CR0013 <123cr0013@tokyo.amg.ac.jp>
2024-02-05 23:10:26 +0900

    Towerクラス パーツの情報管理用のメンバ変数追加。

--- Game/Game/source/Tower.h
+++ Game/Game/source/Tower.h
@@ -8,5 +8,18 @@ public:
 	~Tower();
 
 private:
+	
+	struct TOWER_PARTS_INFO {
+		// この要素が使用中かどうか
+		bool use;
+		// パーツのモデルハンドル
+		int modelHandle;
+		// パーツの位置
+		VECTOR pos;
+		// 各軸における1フレームの回転値
+		VECTOR vRot;
+	};
 
+	// パーツの情報
+	std::vector<TOWER_PARTS_INFO> _partsInfo;
 };
