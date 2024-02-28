000e93a195b91fc30c0b46d06d22f34aa3c0ed0e
saito_ko <123cr0006@tokyo.amg.ac.jp>
2024-02-29 02:33:57 +0900

    Merge branch 'feature/FrameData' of https://github.com/123cr0006-Saito/RunawayIronBall into feature/FrameData
    # Conflicts:
    #	Game/Game/Data/DirectionList/DirectionList.csv
    #	Game/Game/Data/FrameData/Player/FD_MO_PL_Horizontal_third.csv
    #	Game/Game/source/ClassificationEffect.cpp
    #	Game/Game/source/ClassificationEffect.h
    #	Game/Game/source/EffekseerBase.cpp
    #	Game/Game/source/EffekseerBase.h
    #	Game/Game/source/EffekseerPosSynchro.h
    #	Game/Game/source/ModeTest.cpp

--- Game/Game/source/ClassificationEffect.cpp
+++ Game/Game/source/ClassificationEffect.cpp
@@ -3,21 +3,29 @@ ClassificationEffect* ClassificationEffect::_instance = nullptr;
 ClassificationEffect::ClassificationEffect() {
 	_instance = this;
 	// ファイルパス
-	std::string filePath = "Data/FrameData/DirectionList.csv";
+	std::string filePath = "Data/DirectionList/DirectionList.csv";
 	//ファイル読み込み
 	CFile csvFile(filePath);
 	//ファイルが開いていたら実行
 	if (csvFile.Success()) {
 		int c = 0;
 		const char* p = (const char*)csvFile.Data();
-		int size = csvFile.Size(); 
-		std::pair<std::string, int> param;
-		c += GetString(&p[c], &param.first); // エフェクト名を取得
-		c += FindString(&p[c], ',', &p[size]); c++; c += GetDecNum(&p[c], &param.second); // keyを取得
-		c += SkipSpace(&p[c], &p[size]); // 空白やコントロールコードをスキップする
-
-		// データを追加
-		_commandList[param.second] = param.first;
+		int size = csvFile.Size();
+		while (c < size) {
+			int key = 0;
+			std::pair<std::string, int> param;
+			c += GetString(&p[c], &param.first); // エフェクト名を取得
+			c += FindString(&p[c], ',', &p[size]); c++; c += GetDecNum(&p[c], &key); // keyを取得
+			c += FindString(&p[c], ',', &p[size]); c++; c += GetDecNum(&p[c], &param.second); // keyを取得
+			c += SkipSpace(&p[c], &p[size]); // 空白やコントロールコードをスキップする
+			// データを追加
+			_commandList[key] = param;
+		}
+	}
+	else {
+#ifdef _DEBUG
+		MessageBox(NULL, "DirectionList が開けませんでした。", "エラー", MB_OK);
+#endif
 	}
 };
 
@@ -27,39 +35,58 @@ ClassificationEffect::~ClassificationEffect() {
 };
 
 void ClassificationEffect::SetClassification(CommandParam param) {
-	if (param.first == Play_Vibration_X) {
-		// バイブレーション
-	}
-	else if (param.first == Play_Vibration_Y) {
+	if (param.first == Play_Vibration) {
 		// バイブレーション
 	}
 	else if (param.first == Play_SE) {
 		// SE
+		int seName = static_cast<int>(param.second);
+		global._soundServer->DirectPlay(_commandList[seName].first);
 	}
-	else if (param.first == Play_CameraVibration) {
-		// カメラバイブレーション
+	else if (param.first == Play_CameraVibration_X) {
+		// カメラバイブレーション X
+		ScreenVibration::GetInstance()->SetVibrationX(param.second, 20);
 	}
-	else if (param.first == Play_Effect_PC) {
+	else if (param.first == Play_CameraVibration_Y) {
+		// カメラバイブレーション Y
+		ScreenVibration::GetInstance()->SetVibrationY(param.second, 20);
+	}
+	else if (param.first == Play_Effect_Dust) {
 		// エフェクト
-	}
-	else if (param.first == Play_Effect_IC) {
-		// エフェクト
-	}
-	else if (param.first == Play_Effect_IU) {
-		// エフェクト
+		int effectName = static_cast<int>(param.second);
+		ResourceServer::Mult handle = ResourceServer::SearchMult(_commandList[effectName].first.c_str());
+		if (handle.AllNum != 0) {
+			VECTOR pos = VGet(0, 0, 0);
+			float animSpeed = 1.0f / 60.0f * 1000;
+			BoardPolygonDust* dust = new BoardPolygonDust(pos, _commandList[effectName].second, handle.handle, handle.AllNum, animSpeed);
+			EffectManeger::GetInstance()->LoadEffect(dust);
+		}
 	}
 	else if (param.first == Play_Effekseer_PC) {
-		// エフェクシア
+		// エフェクシア プレイヤー中心
+		VECTOR vec = VGet(0,0,0);
+		CreateEffeckseer(param.second, &vec);
 	}
 	else if (param.first == Play_Effekseer_IC) {
-		// エフェクシア
+		// エフェクシア 鉄球中心
+		VECTOR pos = VGet(0, 0, 0);
+		CreateEffeckseer(param.second, &pos);
 	}
 	else if (param.first == Play_Effekseer_IU) {
-		// エフェクシア
+		// エフェクシア 鉄球足元
+		VECTOR pos = VGet(0, 0, 0);
+		CreateEffeckseer(param.second, &pos);
 	}
 	else {
 #ifdef _DEBUG
 		MessageBox(NULL, "エフェクトコマンドが見つかりませんでした。", "エラー", MB_OK);
 #endif
 	}
+};
+
+void ClassificationEffect::CreateEffeckseer(float param, VECTOR* pos) {
+	int effectName = static_cast<int>(param);
+	int handle = ResourceServer::SearchSingle(_commandList[effectName].first.c_str(), ResourceServer::TYPE::Efk);
+	EffekseerBase* effekseer = new EffekseerBase(handle, pos, _commandList[effectName].second);
+	EffectManeger::GetInstance()->LoadEffect(effekseer);
 };
