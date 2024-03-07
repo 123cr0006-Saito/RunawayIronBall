5685893c0b389d49530c09cc9038e8a0eddea2ef
123cr0006-Saito <123cr0006@tokyo.amg.ac.jp>
2024-03-07 13:15:23 +0900

    Heartクラスを追加
    内容
    プレイヤーの回復アイテムを追加
    描画の際、ただあるのだと見た目的に微妙なので回転させながら上下に移動するように作成
    Shpereコリジョンからプレイヤーと当たり判定を取ります。

---
+++ Game/Game/source/Heart.cpp
@@ -0,0 +1,32 @@
+#include "Heart.h"
+
+Heart::Heart(VECTOR pos){
+	_model = ResourceServer::MV1LoadModel("Heart","");
+
+	_sphere.r = 50;
+	//pos‚Í‘«Œ³Šî€‚Ål‚¦‚Ä‚¢‚Ü‚·
+	_pos = VAdd(pos, VGet(0, _sphere.r * 2, 0));
+	_sphere.centerPos = _pos;
+	_currentTime = GetNowCount();
+};
+
+Heart::~Heart() {
+
+};
+
+bool Heart::Process() {
+	float nowTime = GetNowCount() - _currentTime;
+	float oneLapTime = 4 * 1000;
+	float direction = 2.0f * DX_PI_F * (nowTime / oneLapTime);
+	float Sin = sin(direction);
+
+	_sphere.centerPos = VAdd(_pos,VGet(0,_sphere.r * Sin,0));
+	MV1SetPosition(_model, _sphere.centerPos);
+	MV1SetRotationXYZ(_model,VGet(0, direction,0));
+	return true;
+};
+
+bool Heart::Render() {
+	MV1DrawModel(_model);
+	return true;
+};
