1b62d1063570881b8d73aefa37b27dde14525ab3
123CR0013 <123cr0013@tokyo.amg.ac.jp>
2024-03-12 02:24:15 +0900

    Houseクラス：Init関数内で親クラスBuildingBaseクラスのInit関数を呼び出すように修正。

--- Game/Game/source/House.cpp
+++ Game/Game/source/House.cpp
@@ -19,27 +19,7 @@ House::~House()
 
 void House::Init(int modelHandle, VECTOR startPos, VECTOR rotation, VECTOR scale, VECTOR obbLength)
 {
-	// モデルの設定
-	_modelHandle = modelHandle;
-	_pos = startPos;
-	MV1SetPosition(_modelHandle, _pos);
-	MV1SetRotationXYZ(_modelHandle, rotation);
-	MV1SetScale(_modelHandle, scale);
-
-	// 当たり判定の設定
-	// スケール
-	obb.length[0] = obbLength.x * scale.x;
-	obb.length[1] = obbLength.y * scale.y;
-	obb.length[2] = obbLength.z * scale.z;
-	// 回転
-	MATRIX mRot = MGetRotX(rotation.x);
-	mRot = MMult(mRot, MGetRotY(rotation.y));
-	mRot = MMult(mRot, MGetRotZ(rotation.z));
-	obb.dir_vec[0] = VTransform(obb.dir_vec[0], mRot);
-	obb.dir_vec[1] = VTransform(obb.dir_vec[1], mRot);
-	obb.dir_vec[2] = VTransform(obb.dir_vec[2], mRot);
-	// 座標
-	obb.pos = VAdd(startPos, VGet(0.0f, obb.length[1] / 2.0f, 0.0f));
+	BuildingBase::Init(modelHandle, startPos, rotation, scale, obbLength);
 
 	// 破壊処理クラスの初期化
 	_breakObj = NEW BreakObject();
