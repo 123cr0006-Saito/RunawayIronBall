03a58a128865c34270700c002bfd2c9ddbf9e977
saito_ko <123cr0006@tokyo.amg.ac.jp>
2024-03-15 18:32:07 +0900

    クライスターの屋根の関数を修正
    内容
    屋根の配置するJoint変数を追加

--- Game/Game/source/CrystarPattern1.cpp
+++ Game/Game/source/CrystarPattern1.cpp
@@ -20,7 +20,7 @@ void CrystarPattern1::InheritanceInit() {
 
 void CrystarPattern1::AnimInit() {
 
-	_roof = NEW CrystarRoof(ResourceServer::MV1LoadModel("CrystarRoof_glsss","res/Enemy/Crystar/cg_crystar_glass.mv1"), _model);
+	_roof = NEW CrystarRoof(ResourceServer::MV1LoadModel("CrystarRoof_glsss","res/Enemy/Crystar/cg_crystar_glass.mv1"), _model, "joint1");
 
 	//// モーションリストのロード
 	MotionList::Load("Crystarl", "MotionList_Crystarl.csv");
