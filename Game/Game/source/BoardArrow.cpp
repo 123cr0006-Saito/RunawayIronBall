f397cf1001b36826f8656295427dca61aa117ac2
123cr0006 <123cr0006@tokyo.amg.ac.jp>
2024-04-08 20:02:48 +0900

    矢印にアニメーションを付けました。

--- Game/Game/source/BoardArrow.cpp
+++ Game/Game/source/BoardArrow.cpp
@@ -6,33 +6,39 @@
 // 指定の方向に矢印を描画するクラス
 //----------------------------------------------------------------------
 #include "BoardArrow.h"
-const unsigned short ::BoardArrow::vertexList[6] = { 0,1,2,2,1,3 };
+const unsigned short ::BoardArrow::vertexList[12] = {0,1,2,2,1,3,4,5,6,6,5,7};
 //----------------------------------------------------------------------
 // @brief コンストラクタ
 // @param name: ファイル名
 // @param length: 矢印の長さ
 // @return なし
 //----------------------------------------------------------------------
-BoardArrow::BoardArrow(std::string name,float length):
-	_handle(ResourceServer::LoadGraph("Arrow", name.c_str()))
+BoardArrow::BoardArrow(std::string name,float length, int animMax):
+	_handle(ResourceServer::LoadGraph("Arrow", name.c_str())),
+	_animMax(animMax)
 {
 	// 初期化
+	_animCount = 0;
 	// uvリスト
-	float uvList[4][2] = {
-		{0.0 ,0.0 },
-		{1.0f,0.0 },
-		{0.0 ,1.0f},
-		{1.0f,1.0f}
+	float uvList[VERTEX_MAX][2] = {
+		{0.0f,0.0f},
+		{1.0f,0.0f},
+		{0.0f,0.0f},
+		{1.0f,0.0f},
+		{0.0f,1.0f},
+		{1.0f,1.0f},
+		{0.0f,0.0f},
+		{1.0f,0.0f}
 	};
 	
 	// 横の長さ
 	float Width = 50.0f;
 	// 縦の長さ
 	_length = length;
-	float posListX[4] = { -Width,Width, -Width ,Width };
-	float posListZ[4] = { _length,_length,0.0f,0.0f };
+	float posListX[VERTEX_MAX] = { -Width, Width, -Width, Width, -Width, Width, -Width, Width};
+	float posListZ[VERTEX_MAX] = { _length, _length, _length, _length, _length, _length, 0.0f, 0.0f};
 	// 頂点の設定
-	for (int i = 0; i < 4; i++) {
+	for (int i = 0; i < VERTEX_MAX; i++) {
 		_originPos[i] = VGet(posListX[i], 50, posListZ[i]);
 		vertex[i].norm = VGet(0,1,0);
 		vertex[i].dif = GetColorU8(255, 255, 255, 255);
@@ -47,8 +53,21 @@ BoardArrow::BoardArrow(std::string name,float length):
 // @brief デストラクタ
 // @return なし
 //----------------------------------------------------------------------
-BoardArrow::~BoardArrow() {
-
+BoardArrow::~BoardArrow() {};
+//----------------------------------------------------------------------
+// @brief 頂点の更新
+// @return なし
+//----------------------------------------------------------------------
+void BoardArrow::UpdateVertex() {
+	float Width = 50.0f;
+	float ratio = static_cast<float>(_animCount) / _animMax;
+	for (int i = 0; i < 2; i++) {
+		VECTOR dirVec = _originPos[i];
+		_originPos[i + 2].z = VScale(dirVec, ratio).z;
+		_originPos[i + 4].z = VScale(dirVec, ratio).z;
+		vertex[i].v = 1.0f - ratio;
+		vertex[i+6].v = 1.0f - ratio;
+	}
 };
 //----------------------------------------------------------------------
 // @brief 更新処理
@@ -56,10 +75,17 @@ BoardArrow::~BoardArrow() {
 // @param dirY: y軸の回転角度
 // @return 成功したかどうか
 //----------------------------------------------------------------------
-bool BoardArrow::Update(VECTOR pos, float dirY) {
+bool BoardArrow::Process(VECTOR pos, float dirY) {
+	// アニメーションを進める
+	_animCount++;
+	if (_animCount > _animMax) {
+		_animCount = 0;
+	}
+	// 頂点情報を更新
+	UpdateVertex();
 	// y軸行列を使用し回転させる
 	MATRIX matrix = MGetRotY(dirY);
-	for (int i = 0; i < 4; i++) {
+	for (int i = 0; i < VERTEX_MAX; i++) {
 		vertex[i].pos = VAdd(pos,VTransform(_originPos[i], matrix));
 	}
 	return true;
@@ -70,11 +96,18 @@ bool BoardArrow::Update(VECTOR pos, float dirY) {
 // @param dirVec: 方向ベクトル
 // @return 成功したかどうか
 //----------------------------------------------------------------------
-bool BoardArrow::Update(VECTOR pos, VECTOR dirVec) {
+bool BoardArrow::Process(VECTOR pos, VECTOR dirVec) {
 	// 方向ベクトルから角度を出し、y軸行列で回転させる
 	float dirY = atan2(dirVec.x, dirVec.z);
+	// アニメーションを進める
+	_animCount++;
+	if (_animCount > _animMax) {
+		_animCount = 0;
+	}
+	// 頂点情報を更新
+	UpdateVertex();
 	MATRIX matrix = MGetRotY(dirY);
-	for (int i = 0; i < 4; i++) {
+	for (int i = 0; i < VERTEX_MAX; i++) {
 		vertex[i].pos = VAdd(pos, VTransform(_originPos[i], matrix));
 	}
 	return true;
@@ -84,6 +117,6 @@ bool BoardArrow::Update(VECTOR pos, VECTOR dirVec) {
 // @return 成功したかどうか
 //----------------------------------------------------------------------
 bool BoardArrow::Render() {
-	DrawPolygonIndexed3D(vertex, VERTEX_MAX, vertexList,  2, _handle, true);
+	DrawPolygonIndexed3D(vertex, VERTEX_MAX, vertexList, 4, _handle, true);
 	return true;
 };
