ddeb5a9556b3a7a6058364809cbf4006e75696e3
123CR0013 <123CR0013@tokyo.amg.ac.jp>
2024-04-10 00:39:07 +0900

    コードを整理。コメントアウトを追加。

--- Game/Game/source/Tower.cpp
+++ Game/Game/source/Tower.cpp
@@ -9,22 +9,23 @@
 #include "Tower.h"
 
 namespace {
-	// 落下処理のカウント
-	constexpr int FALL_CNT_MAX = 30; 
+	// プレイヤーからの攻撃判定を無効化するフレーム数
+	constexpr int INVINCIBLE_CNT_MAX = 45; 
 }
 
 Tower::Tower() : ObjectBase::ObjectBase()
 {
 	_use = true;
 	_pos = VGet(0.0f, 0.0f, 0.0f);
+
+	_towerParts.clear();
 	_partsNum = 0;
-	_isFalling = false;
-	_prevFallCnt = -1;
-	_fallCnt = 0;
-
-	_canBlast = true;
-
 	_bottomIndex = 0;
+
+	_checkFallEnd = false;
+	_invincibleCnt = -1;
+
+	_canBlast = true;	
 
 	_sphereCollision.centerPos = VGet(0.0f, 0.0f, 0.0f);
 	_sphereCollision.r = 0.0f;
@@ -38,21 +39,25 @@ Tower::~Tower()
 	_towerParts.clear();
 }
 
+// 初期化処理
+// @param modelHandle: モデルハンドル（配列の要素数がタワーの段数となる）
+// @param startPos: 起点座標
+// @param rotation: モデルの回転値
+// @param scale: モデルの拡大率
 void Tower::Init(std::vector<int> modelHandle, VECTOR startPos, VECTOR rotation, VECTOR scale)
 {
 	_pos = startPos;
-	_sphereCollision.centerPos = _pos;
-	_sphereCollision.r = 250.0f;
-	_cell->_objType = OBJ_TYPE::TWR;
-	_collisionManager->UpdateCell(_cell);
 
 	_partsNum = modelHandle.size();
+	// タワーパーツを生成
 	for (int i = 0; i < _partsNum; i++) {
 		TowerParts* tp = NEW TowerParts();
 		VECTOR tmpPos = VGet(0.0f, 0.0f, 0.0f);
+		// 最下部のパーツは、startPosに配置する
 		if (i == 0) {
 			tmpPos = startPos;
 		}
+		// それ以外のパーツは、一つ下のパーツのソケット（ジョイント）座標に配置する
 		else {
 			VECTOR vOrigin = VGet(0.0f, 0.0f, 0.0f);
 			MATRIX m = MV1GetFrameLocalWorldMatrix(_towerParts[i - 1]->_modelHandle, 1);
@@ -63,14 +68,19 @@ void Tower::Init(std::vector<int> modelHandle, VECTOR startPos, VECTOR rotation,
 		_towerParts.push_back(tp);
 	}
 
-	
+	// タワー本体の当たり判定を設定
+	_sphereCollision.centerPos = _pos;
+	_sphereCollision.r = 250.0f;
+	_cell->_objType = OBJ_TYPE::TWR;
+	_collisionManager->UpdateCell(_cell);
 }
 
+// 更新処理
 void Tower::Process()
 {
 	if (_use) {
-		// 落下処理
-		if (_isFalling) {
+		// パーツの落下終了判定
+		if (_checkFallEnd) {
 			// 残っている全てのパーツが落下終了したかどうかを判定する
 			bool finishedFalling = true;
 			for (int i = _bottomIndex; i < _partsNum; i++) {
@@ -78,28 +88,32 @@ void Tower::Process()
 
 				finishedFalling = finishedFalling && !(_towerParts[i]->GetIsFalling());
 			}
-			// 全てのパーツが落下終了したら、落下処理を終了する
+			// 全てのパーツが落下終了したら
 			if (finishedFalling) {
-				_isFalling = false;
+				// 落下終了判定をやめる
+				_checkFallEnd = false;
+				// パーツを吹き飛ばし可能状態にする
 				_canBlast = true;
 			}
 		}
 
-
-		if (_prevFallCnt >= 0) {
-			_prevFallCnt--;
-			if (_prevFallCnt < 0) {
-				_isFalling = true;
-				_fallCnt = FALL_CNT_MAX;
+		// プレイヤーからの攻撃判定を無効化中の処理
+		if (_invincibleCnt >= 0) {
+			_invincibleCnt--;
+			if (_invincibleCnt < 0) {
+				_checkFallEnd = true;
 			}
 		}
 
 	}
+
+	// パーツの更新処理
 	for (int i = 0; i < _partsNum; i++) {
 		_towerParts[i]->Process();
 	}
 }
 
+// 描画処理
 void Tower::Render()
 {
 	for (int i = 0; i < _partsNum; i++) {
@@ -107,26 +121,29 @@ void Tower::Render()
 	}
 }
 
+// タワーパーツの吹き飛ばし設定
+// @param vDir: 吹き飛ばす方向
 void Tower::SetBlast(VECTOR vDir)
 {
-	if (_prevFallCnt < 0 && !_isFalling) {
-		_prevFallCnt = 45;
+	// タワーが吹き飛ばし可能状態なら
+	if (_canBlast) {
+		_invincibleCnt = INVINCIBLE_CNT_MAX;
 		_canBlast = false;
 
 		for (int i = _bottomIndex; i < _partsNum; i++) {
-
 			// 最下部のパーツのみ吹っ飛び処理
 			if (i == _bottomIndex) {
 				_towerParts[i]->SetBlast(VNorm(vDir));
 			}
 			// それ以外のパーツは落下処理
 			else {
-				// 一つしたのパーツの現在の座標を、落下終了後の座標として設定
+				// 一つ下のパーツの吹き飛ばし前の座標を、落下終了後の座標として設定
 				_towerParts[i]->SetFalling(_towerParts[i - 1]->_pos);
 			}
 		}
 		_bottomIndex++;
 
+		//パーツが残っていなかったら、タワー本体の当たり判定を無効化
 		if (_bottomIndex >= _partsNum) {
 			_use = false;
 			_collisionManager->ReserveRemovementCell(_cell);
@@ -134,6 +151,7 @@ void Tower::SetBlast(VECTOR vDir)
 	}
 }
 
+// デバッグ情報の表示
 void Tower::DrawDebugInfo()
 {
 	if (_use) {
