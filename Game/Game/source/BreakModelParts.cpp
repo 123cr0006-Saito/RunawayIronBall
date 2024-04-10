a14fa5d4fb4390a1cabefe91eb5a6e1e2fd4dcbf
123CR0013 <123CR0013@tokyo.amg.ac.jp>
2024-04-10 15:21:15 +0900

    コードを整理。コメントアウトを追加。

--- Game/Game/source/BreakModelParts.cpp
+++ Game/Game/source/BreakModelParts.cpp
@@ -1,3 +1,12 @@
+//----------------------------------------------------------------------
+// @filename BreakModelParts.cpp
+// ＠date: 2024/04/01
+// ＠author: Morozumi Hiroya
+// @explanation
+// 建物モデルをパーツごとに吹き飛ばす処理を行うクラス
+// あらかじめモデルのパーツを分割しておき、そのパーツ分けを利用して吹き飛ばす処理を行う
+// パーツの名前の先頭に「BRK」がついているものを吹き飛ばす対象とし、それ以外のパーツはその場にとどまる 
+//----------------------------------------------------------------------
 #include "BreakModelParts.h"
 
 BreakModelParts::BreakModelParts()
@@ -6,8 +15,7 @@ BreakModelParts::BreakModelParts()
 	_finishedBreakAll = false;
 
 	_modelHandle = -1;
-
-	_breakCnt = 0;
+	_modelRotation = VGet(0.0f, 0.0f, 0.0f);
 
 	_blastDir = VGet(0.0f, 0.0f, -1.0f);
 
@@ -29,70 +37,42 @@ void BreakModelParts::Init(int modelHandle)
 		// フレームの名前を取得する
 		std::string frameName = MV1GetFrameName(_modelHandle, i);
 		std::string checkName = "BRK";
-		// フレーム名がBRKで始まらない場合はcontinue
+		// フレーム名が「BRK」で始まらない場合はcontinue
 		if (frameName.substr(0, 3) != checkName) {
 			continue;
 		}
 
-
-		// フレームの座標変換行列を取得する
-		MATRIX mFrameLocal = MV1GetFrameLocalMatrix(_modelHandle, i);
-		// フレームのローカル座標を求める
-		VECTOR vFrameLocalPos = VGet(0.0f, 0.0f, 0.0f);
-		vFrameLocalPos = VTransform(vFrameLocalPos, mFrameLocal);
-
-		// モデルの起点座標から見たフレームの方向を取得する
-		VECTOR vFrameLocalDir = VGet(0.0f, 0.0f, -1.0f);
-		if (VSquareSize(vFrameLocalPos) > 0.0000f) {
-			vFrameLocalDir = VNorm(vFrameLocalPos);
-			// 吹っ飛ぶ方向をz軸マイナス方向に限定する
-			if (vFrameLocalDir.z > 0.0f) {
-				vFrameLocalDir.z *= -1;
-			}
-		}
-
 		FRAME_INFO* f = NEW FRAME_INFO();
 		f->frameIndex = i;
-		f->finishedBreak;
-		f->horizontalDir = vFrameLocalDir;
+		f->finishedBreak = false;
+		f->horizontalDir = VGet(0.0f, 0.0f, -1.0f);
 		f->horizontalVelocity = 0.0f;
 		f->verticalVelocity = 0.0f;
 		f->vRot = VGet(0.0f, 0.0f, 0.0f);
 
 		_frameInfo.push_back(f);
-
-
-
-
-
-		_locus.resize(_locus.size() + 1);
 	}
 
-
-
-
-
-
 	// デバッグ用
-
+	_locus.resize(_frameInfo.size());
 	for (auto itr = _frameInfo.begin(); itr != _frameInfo.end(); ++itr) {
 		MATRIX m = MV1GetFrameLocalWorldMatrix(_modelHandle, (*itr)->frameIndex);
 
 		VECTOR v = VTransform(VGet(0.0f, 0.0f, 0.0f), m);
 		_locus.at(std::distance(_frameInfo.begin(), itr)).push_back(v);
 	}
-
-	// 吹っ飛ばす方向を指定
-	SetBlastDir(VGet(1.0f, 0.0f, 0.0f));
 }
 
+// 更新処理
 void BreakModelParts::Process()
 {
+	// 吹き飛ばし処理
+	// 有効状態かつ、吹き飛ばし処理が終了していないパーツがある場合
 	if (!_finishedBreakAll && _isActive) {
 		_finishedBreakAll = true;
 		// 破片が飛び散る処理
 		for (auto itr = _frameInfo.begin(); itr != _frameInfo.end(); ++itr) {
-
+			// すでに吹き飛ばし処理が終了している場合はスキップ
 			if((*itr)->finishedBreak) continue;
 			_finishedBreakAll = false;
 
@@ -113,52 +93,24 @@ void BreakModelParts::Process()
 			MV1SetFrameUserLocalMatrix(_modelHandle, (*itr)->frameIndex, m);
 
 
-
-			// 重力処理
-			(*itr)->verticalVelocity -= 2.f;
-
-			// 軌跡表示用の座標を保持
-			MATRIX mLocus = MV1GetFrameLocalWorldMatrix(_modelHandle, (*itr)->frameIndex);
-			VECTOR vLocus = VTransform(VGet(0.0f, 0.0f, 0.0f), mLocus);
-			_locus.at(std::distance(_frameInfo.begin(), itr)).push_back(vLocus);
-
-			if (vLocus.y < 0.0f) {
+			// パーツ吹き飛ばし処理の終了判定
+			MATRIX mPivot = MV1GetFrameLocalWorldMatrix(_modelHandle, (*itr)->frameIndex);
+			VECTOR pivotPos = VTransform(VGet(0.0f, 0.0f, 0.0f), mPivot);
+			// パーツごとのピボット座標が地面より下にある場合、吹き飛ばし処理を終了する
+			if (pivotPos.y < 0.0f) {
 				(*itr)->finishedBreak = true;
 			}
 
-			//itr->pos = v;
+			// 重力加速
+			(*itr)->verticalVelocity -= 2.f;
+
+
+
+			// 軌跡表示用の座標を保持
+			_locus.at(std::distance(_frameInfo.begin(), itr)).push_back(pivotPos);
 		}
-		//_blastPower -= 0.5f;
-
-		_breakCnt++;
-		//_blastDir.y -= 0.05f;
-
-		// // リセット
-		//if (_breakCnt > 180) {
-		//	_finishedBreakAll = true;
-
-		//	//_breakCnt = 0;
-
-		//	//// 吹っ飛ばす方向を指定
-		//	//SetBlastDir(VGet(0.0f, 0.0f, 1.0f));
-
-		//	//// 軌跡表示用の座標情報をリセット
-		//	//for (auto itr = _locus.begin(); itr != _locus.end(); ++itr) {
-		//	//	itr->clear();
-		//	//}
-
-		//	//// パーツを初期位置に戻す
-		//	//for (auto itr = _frameInfo.begin(); itr != _frameInfo.end(); ++itr) {
-		//	//	MV1ResetFrameUserLocalMatrix(_modelHandle, itr->frameIndex);
-
-		//	//	MATRIX m = MV1GetFrameLocalWorldMatrix(_modelHandle, itr->frameIndex);
-		//	//	VECTOR v = VTransform(VGet(0.0f, 0.0f, 0.0f), m);
-		//	//	_locus.at(std::distance(_frameInfo.begin(), itr)).push_back(v);
-		//	//}
-		//}
-
-
 	}
+
 	// 軌跡表示のOn/Off切り替え
 	auto input = XInput::GetInstance();
 	if (input->GetTrg(XINPUT_BUTTON_BACK) != 0) {
@@ -166,29 +118,27 @@ void BreakModelParts::Process()
 	}
 }
 
-void BreakModelParts::Render()
-{
-
-}
-
-void BreakModelParts::Activate(bool activate, VECTOR _blastDir)
+// 吹っ飛び処理の有効化設定
+// @param activate : 吹っ飛び処理を有効化するかどうか
+// @param _blastDir : 吹っ飛ばしの方向
+void BreakModelParts::Activate(bool activate, VECTOR blastDir)
 {
 	if (!activate) {
 		ResetFrameMatrix();
 	}else if (!_isActive) { // 現在が有効状態でなく、新しく有効化する場合にのみ吹っ飛ばしの設定を行う	
-		SetBlastDir(_blastDir);
+		SetBlastDir(blastDir);
 	}
 	_isActive = activate;
 }
 
 // 吹っ飛ばしの方向をセットする
-// 引数
-// vDir : ふっ飛ばしの中心方向
+// パーツごとに、吹っ飛ばしの方向と1フレームあたりの回転地を設定する
+// @param vDir : 吹っ飛ばしの中心方向
 void BreakModelParts::SetBlastDir(VECTOR vDir)
 {
 	_modelRotation = MV1GetRotationXYZ(_modelHandle);
 
-
+	// XZ平面上で方向を指定する
 	vDir.y = 0.0f;
 	vDir = VNorm(vDir);
 
@@ -196,51 +146,70 @@ void BreakModelParts::SetBlastDir(VECTOR vDir)
 	MATRIX mToLocal = MGetRotY(-_modelRotation.y);
 	VECTOR vLocalDir = VTransform(vDir, mToLocal);
 
-	// パーツごとに吹っ飛ばす水平方向をvDirから ±maxRange度の間でランダムに決定する
+	// 吹っ飛ばしの水平方向パーツごとに、vLocalDirから±maxRange度の間でランダムに決定する
 	const int maxRange = 35;
 	// 水平・鉛直方向における最大速度
 	const int maxHorizontalVelocity = 80;
 	const int maxVerticalVelocity = 50;
 
-	// パーツごとにふっ飛ばし方向をセットする
-
-
+	// パーツごとにふっ飛ばし方向と速度をセットする
 	for (auto itr = _frameInfo.begin(); itr != _frameInfo.end(); ++itr) {
-		// 水平方向
+		// ------------------------------
+		// 水平方向の設定
+
+		// vLocalDirから一定範囲内でランダムに方向を決定する
 		float angle = rand() % (maxRange * 2);
 		angle -= maxRange;
-		
-
+		// 方向
 		(*itr)->horizontalDir = VTransform(vLocalDir, MGetRotY(Math::DegToRad(angle)));
+		// 速度
 		(*itr)->horizontalVelocity = rand() % maxHorizontalVelocity;
+		(*itr)->horizontalVelocity = Math::Clamp(20.0f, maxHorizontalVelocity, (*itr)->horizontalVelocity);
+		// ------------------------------
 
-		// 鉛直方向
-		(*itr)->verticalVelocity = (rand() % maxVerticalVelocity) + 20.0f;
+		// ------------------------------
+		// 鉛直方向の設定
 
-		// 回転値
-		int deltaRot = 4;
+		// 速度
+		(*itr)->verticalVelocity = rand() % maxVerticalVelocity;
+		(*itr)->verticalVelocity = Math::Clamp(20.0f, maxVerticalVelocity, (*itr)->verticalVelocity);
+		// ------------------------------
 
-		float angleX = rand() % deltaRot;
+		// ------------------------------
+		// 1フレーム当たりの回転値を軸ごとにランダムに決定する
+
+		// 回転値の最大
+		int deltaRotMax = 4;
+
+		// X軸の回転値
+		float angleX = rand() % deltaRotMax;
 		angleX = Math::DegToRad(angleX);
+		// 回転方向をランダムに決定
 		int mul = rand() % 2 == 0 ? 1 : -1;
 		angleX *= mul;
 
-		float angleY = rand() % deltaRot;
+		// Y軸の回転値
+		float angleY = rand() % deltaRotMax;
 		angleY = Math::DegToRad(angleY);
+		// 回転方向をランダムに決定
 		mul = rand() % 2 == 0 ? 1 : -1;
 		angleY *= mul;
 
-		float angleZ = rand() % deltaRot;
+		// Z軸の回転値
+		float angleZ = rand() % deltaRotMax;
 		angleZ = Math::DegToRad(angleZ);
+		// 回転方向をランダムに決定
 		mul = rand() % 2 == 0 ? 1 : -1;
 		angleZ *= mul;
 
+		// 1フレームあたりの回転値をセット
 		(*itr)->vRot = VGet(angleX, angleY, angleZ);
+		// ------------------------------
 	}
-	//_blastPower = 20.0f;
 	_blastDir = vDir;
 }
 
+// パーツごとに適応した行列をリセットし、モデルを初期状態に戻す
 void BreakModelParts::ResetFrameMatrix()
 {
 	for (auto itr = _frameInfo.begin(); itr != _frameInfo.end(); ++itr) {
@@ -248,6 +217,7 @@ void BreakModelParts::ResetFrameMatrix()
 	}
 }
 
+// デバッグ情報の表示
 void BreakModelParts::DrawDebugInfo()
 {
 	if (_isActive && _isDrawLocus) {
--- Game/Game/source/BreakModelParts.h
+++ Game/Game/source/BreakModelParts.h
@@ -1,3 +1,12 @@
+//----------------------------------------------------------------------
+// @filename BreakModelParts.h
+// ＠date: 2024/04/01
+// ＠author: Morozumi Hiroya
+// @explanation
+// 建物モデルをパーツごとに吹き飛ばす処理を行うクラス
+// あらかじめモデルのパーツを分割しておき、そのパーツ分けを利用して吹き飛ばす処理を行う
+// パーツの名前の先頭に「BRK」がついているものを吹き飛ばす対象とし、それ以外のパーツはその場にとどまる 
+//----------------------------------------------------------------------
 #pragma once
 #include "appframe.h"
 
@@ -7,33 +16,36 @@ public:
 	BreakModelParts();
 	~BreakModelParts();
 
+	// 初期化処理
 	void Init(int modelHandle);
-
+	// 更新処理
 	void Process();
-	void Render();
 
-
+	// 吹き飛ばし処理の有効状態かどうかを取得する
 	bool GetIsActive() { return _isActive; }
-	void Activate(bool activate, VECTOR _blastDir = VGet(0.0f,0.0f,0.0f));
 
+	// 吹き飛ばし処理の有効化設定
+	// @param activate : 吹き飛ばし処理を有効化するかどうか
+	// @param _blastDir : 吹き飛ばし方向
+	void Activate(bool activate, VECTOR blastDir = VGet(0.0f,0.0f,0.0f));
 
-
-
+	// デバッグ情報の表示
 	void DrawDebugInfo();
-
-
 
 private:
 
-	// 吹っ飛ばしの中心方向をセットする
+	// 吹き飛ばし方向をセットする
+	// パーツごとに、吹き飛ばし方向と1フレームあたりの回転地を設定する
+	// @param vDir : 吹き飛ばしの中心方向
 	void SetBlastDir(VECTOR vDir);
+	// パーツごとに適応した行列をリセットし、モデルを初期状態に戻す
 	void ResetFrameMatrix();
 
 
 private:
-	// パーツの吹っ飛び処理を行うかどうか
+	// パーツの吹き飛ばし処理を行うかどうか
 	bool _isActive;
-	// パーツの吹っ飛び処理が終了したかどうか
+	// パーツの吹き飛ばし処理が終了したかどうか
 	bool _finishedBreakAll;
 
 	// モデルハンドル
@@ -42,29 +54,28 @@ private:
 	// モデルの回転値
 	VECTOR _modelRotation;
 
-	int _breakCnt;
-
 	struct FRAME_INFO {
 		// 3Dモデルのパーツのインデックス番号
 		int frameIndex;
 
+		// 吹き飛ばし処理が終了したかどうか
 		bool finishedBreak;
 
+		// --------------------------------------------------------------------------------------------
 		// 吹き飛ばし情報
 		VECTOR horizontalDir; // 水平方向
 		float horizontalVelocity; // 水平方向の速度
 		float verticalVelocity; // 鉛直方向の速度
 		VECTOR vRot; // 各軸における1フレームの回転値
+		// --------------------------------------------------------------------------------------------
 	};
 
 	std::vector<FRAME_INFO*> _frameInfo;
 
 
-	/*  */
-	// 吹っ飛び方向
+	// デバッグ表示用
+	// 吹き飛ばし方向
 	VECTOR _blastDir;
-
-
 	// 軌跡表示をするかどうか
 	bool _isDrawLocus;
 	// 軌跡表示用の座標情報
