d2523076413a2ff3d062049aa7fb4a1fc9a2457a
123CR0013 <123CR0013@tokyo.amg.ac.jp>
2024-04-10 11:45:02 +0900

    コードを整理。コメントアウトを追加。

--- Game/Game/source/BuildingBase.cpp
+++ Game/Game/source/BuildingBase.cpp
@@ -1,8 +1,20 @@
+//----------------------------------------------------------------------
+// @filename BuildingBase.cpp
+// ＠date: 2024/04/01
+// ＠author: Morozumi Hiroya
+// @explanation
+// 建物クラスの基底クラス
+//----------------------------------------------------------------------
 #include "BuildingBase.h"
 
 BuildingBase::BuildingBase()
 {
 	_modelHandle = -1;
+
+	_hp = 0;
+	_exp = 0;
+	_suppression = 0;
+
 	_pos = VGet(0.0f, 0.0f, 0.0f);
 	_useCollision = true;
 	_canBreak = false;
@@ -12,6 +24,7 @@ BuildingBase::~BuildingBase()
 {
 }
 
+// 初期化処理
 void BuildingBase::Init(int modelHandle, std::string objName, VECTOR startPos, VECTOR rotation, VECTOR scale, VECTOR obbLength)
 {
 	// モデルの設定
@@ -42,16 +55,19 @@ void BuildingBase::Init(int modelHandle, std::string objName, VECTOR startPos, V
 	_collisionManager->UpdateCell(_cell);
 }
 
+// 更新処理
 void BuildingBase::Process()
 {
 
 }
 
+// 描画処理
 void BuildingBase::Render()
 {
 	MV1DrawModel(_modelHandle);
 }
 
+// 当たり判定の使用設定
 void BuildingBase::SetUseCollision(bool b)
 {
 	_useCollision = b;
@@ -63,6 +79,7 @@ void BuildingBase::SetUseCollision(bool b)
 	}
 }
 
+// デバッグ情報の表示
 void BuildingBase::DrawDebugInfo()
 {
 	if (_useCollision) {
--- Game/Game/source/BuildingBase.h
+++ Game/Game/source/BuildingBase.h
@@ -1,3 +1,10 @@
+//----------------------------------------------------------------------
+// @filename BuildingBase.h
+// ＠date: 2024/04/01
+// ＠author: Morozumi Hiroya
+// @explanation
+// 建物クラスの基底クラス
+//----------------------------------------------------------------------
 #pragma once
 #include "appframe.h"
 #include "ObjectBase.h"
@@ -9,24 +16,31 @@ public:
 	BuildingBase();
 	virtual ~BuildingBase();
 
+	// 初期化処理
 	virtual void Init(int modelHandle, std::string objName,VECTOR startPos, VECTOR rotation, VECTOR scale, VECTOR obbLength);
+	// 更新処理
 	virtual void Process();
+	// 描画処理
 	virtual void Render();
+
+	std::string GetName() { return _objName; }
+	virtual int GetHp() { return 0; }
+	virtual int GetExp() { return 0; }
+	int GetSuppression() { return _suppression; }
 
 	VECTOR GetPos() { return _pos; }
 
-	bool GetUseCollision() { return _useCollision; }
-	void SetUseCollision(bool b);
 	bool GetCanBreak() { return _canBreak; }
+
+	// 当たり判定の使用設定
+	void SetUseCollision(bool b);
+
+	bool GetUseCollision() { return _useCollision; }
 	OBB GetOBBCollision() { return _obbCollision; }
 
-	std::string GetName() {return _objName;}
-	int GetSuppression() { return _suppression; }
-
-
+	// 被ダメージ設定
+	// Houseクラスでオーバーライド
 	virtual void SetHit(VECTOR vDir = VGet(0.0f, 0.0f, -1.0f)) {};
-	virtual int GetExp() { return 0; }
-	virtual int GetHp() { return 0; }
 
 	// デバッグ情報の表示
 	virtual void DrawDebugInfo();
@@ -41,10 +55,12 @@ protected:
 	// モデルの座標
 	VECTOR _pos;
 
+	// 壊せるオブジェクトかどうか
+	// 継承先で設定
+	bool _canBreak;
+
 	// 当たり判定を行うかどうか
 	bool _useCollision;
-	// 壊せるオブジェクトかどうか
-	bool _canBreak;
 	// 当たり判定
 	OBB _obbCollision;
 };
