316023f185b15e46f853027062f174fef70a0bd7
123CR0013 <123CR0013@tokyo.amg.ac.jp>
2024-05-07 01:04:08 +0900

    ObjectBaseクラス：コメントアウトを追加。コードを整理。

--- Game/Game/source/ObjectBase.cpp
+++ Game/Game/source/ObjectBase.cpp
@@ -1,14 +1,26 @@
+//----------------------------------------------------------------------
+// @filename ObjectBase.cpp
+// @date: 2024/03/07
+// @author: Morozumi Hiroya
+// 
+// @explanation
+// 当たり判定処理を行う全てのオブジェクトの基底クラス
+// 当たり判定処理の管理に使用するCoollisionManagerクラスとCellクラスのインスタンスを保持する
+// （CollisionManagerクラス：XZ平面に対して4分木空間分割を行い、当たり判定処理の回数を削減するためのクラス）
+// （Cellクラス：CollisionManagerクラスに情報を登録するためのクラス）
+//----------------------------------------------------------------------
 #include "ObjectBase.h"
 #include "Application/UtilMacro.h"
 
 ObjectBase::ObjectBase()
 {
+	_collisionManager = CollisionManager::GetInstance();
 	_cell = NEW Cell();
 	_cell->_obj = this;
-	_collisionManager = CollisionManager::GetInstance();
 }
 
 ObjectBase::~ObjectBase()
 {
+	_collisionManager = nullptr;
 	SAFE_DELETE(_cell);
 }
--- Game/Game/source/ObjectBase.h
+++ Game/Game/source/ObjectBase.h
@@ -1,9 +1,17 @@
+//----------------------------------------------------------------------
+// @filename ObjectBase.h
+// @date: 2024/03/07
+// @author: Morozumi Hiroya
+// 
+// @explanation
+// 当たり判定処理を行う全てのオブジェクトの基底クラス
+// 当たり判定処理の管理に使用するCoollisionManagerクラスとCellクラスのインスタンスを保持する
+// （CollisionManagerクラス：XZ平面に対して4分木空間分割を行い、当たり判定処理の回数を削減するためのクラス）
+// （Cellクラス：CollisionManagerクラスに情報を登録するためのクラス）
+//----------------------------------------------------------------------
 #pragma once
 #include "Cell.h"
 #include "CollisionManager.h"
-//class CharacterBase;
-//class Player;
-//class EnemyBase;
 
 class CollisionManager;
 class Cell;
@@ -14,6 +22,8 @@ public:
 	ObjectBase();
 	virtual ~ObjectBase();
 
+	// CollisionManagerクラスのインスタンス
 	CollisionManager* _collisionManager;
+	// Cellクラスのインスタンス
 	Cell* _cell;
 };
